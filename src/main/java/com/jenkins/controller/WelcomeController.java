ba82ad9fc7a458b9d38d965f50e56e9e5efb3693
Ashish R <ashish@softsuave.com>
2021-03-10 20:50:52 +0530

    Trigger build test 8

--- src/main/java/com/jenkins/controller/WelcomeController.java
+++ src/main/java/com/jenkins/controller/WelcomeController.java
@@ -1,21 +1,20 @@
 package com.jenkins.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class WelcomeController {
 
-	@GetMapping
-	public ResponseEntity<?> welcome() {
-		return ResponseEntity.ok("Welcome to LearnJenkins Maven Project");
-	}
+//	@GetMapping
+//	public ResponseEntity<?> welcome() {
+//		return ResponseEntity.ok("Welcome to LearnJenkins Maven Project");
+//	}
 	
-	@GetMapping("/home")
-	public String welcomeHome() {
-		return "welcome.jsp";
+	@GetMapping("/")
+	public String welcomeHome(Model model) {
+		model.addAttribute("name", "Ashish");
+		return "welcome";
 	}
 }
