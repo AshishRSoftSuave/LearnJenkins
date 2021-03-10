deb1de6db29f7c7098823fe1c4758c02c4712814
Ashish R <ashish@softsuave.com>
2021-03-10 16:58:39 +0530

    Initial Commit

---
+++ src/main/java/com/jenkins/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.jenkins.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+
+	@GetMapping
+	public ResponseEntity<?> welcome() {
+		return ResponseEntity.ok("Welcome to LearnJenkins Maven Project");
+	}
+}
